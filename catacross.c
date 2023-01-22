8aa6e912812597078001b2142b170f8c9e1a1fbe
lewisrobson <50459012+lewisrobson@users.noreply.github.com>
2023-01-22 23:36:35 +0000

    padding.c has been written
    automatically see's if padding is required and prompts the user

--- catacross.c
+++ catacross.c
@@ -65,14 +65,14 @@ FILE *fp_left, *fp_right, *fp_left_backup, *fp_right_backup;
 fp_left = fopen(argv[1], "r");
 if(fp_left == NULL){
     printf("error opening file %s\n", argv[1]);
-    fclose(fp_left);
+    //fclose(fp_left);
     exit(1);
 }
 
 fp_right = fopen(argv[2], "r");
 if(fp_right == NULL){
     printf("error opening file %s\n", argv[2]);
-    fclose(fp_right);
+    //fclose(fp_right);
     exit(1);
 }
 
@@ -91,9 +91,9 @@ fclose(fp_right);
 
 //########################################################################
 // padding
-/* here, we check if the files are not equal size, If they aren’t, prompt the user 
-for input to take a backup, attempt to automatically create, if the file name we are 
-going for exists, prompt the user for an absolute path and a filename, see example text
+/*       here, we check if the files are not equal size, If they aren’t, prompt the user 
+    for input to take a backup, attempt to automatically create, if the file name we are 
+ going for exists, prompt the user for an absolute path and a filename, see example text
 We then read from the left and right file pointers and write them to left and right file 
 pointers named backup */
 
@@ -106,8 +106,8 @@ if(left_total != right_total){
     if((strcmp(user_selection, "y") == 0) || strcmp(user_selection, "Y") == 0){
         printf("Okay, lets take some backups first\n");
         //do backup of fp_left and fp_right
-        char left_backup[256] = "";
-        char right_backup[256] = "";
+        char left_backup[256] = {0};
+        char right_backup[256] = {0};
         strcat(left_backup, argv[1]);
         strcat(right_backup, argv[2]);
         strcat(left_backup, ".bak");
@@ -148,13 +148,13 @@ if(left_total != right_total){
             fp_left = fopen(argv[1], "r");
             if(fp_left == NULL){
                 printf("error opening file %s\n", argv[1]);
-                fclose(fp_left);
+                //fclose(fp_left);
                 exit(1);
             }
             fp_left_backup = fopen(left_backup, "w");
             if(fp_left_backup == NULL){
                 printf("error opening file %s\n", left_backup);
-                fclose(fp_left_backup);
+                //fclose(fp_left_backup);
                 exit(1);
             }
             char buffer [1024];
@@ -165,20 +165,20 @@ if(left_total != right_total){
             }
             fclose(fp_left_backup);
             fclose(fp_left);
-            //create backup file with new path
+            //     create backup file with new path
             //copy from fp_left into fp_left_backup
 
             //read from fp_right and write into fp_right_backup
             fp_right = fopen(argv[2], "r");
             if(fp_right == NULL){
                 printf("error opening file %s\n", argv[2]);
-                fclose(fp_right);
+                //fclose(fp_right);
                 exit(1);
             }
             fp_right_backup = fopen(right_backup, "w");
             if(fp_right_backup == NULL){
                 printf("error opening file %s\n", right_backup);
-                fclose(fp_right_backup);
+                //fclose(fp_right_backup);
                 exit(1);
             }
         
@@ -198,8 +198,8 @@ if(left_total != right_total){
         }
 }
 
-/////////////// do the padding <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
+/////////////// do the padding <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+padding(left_total, right_total, &argv[0]);
 //########################################################################
 // re open file descriptors to undertake the actual merging
 
@@ -218,7 +218,7 @@ if(fp_right ==NULL){
 char left_items[left_total][256];
 char right_items[right_total][256];
 
-/*these next two loops loop through each line of the file_pointer(s)
+/* these next two loops loop through each line of the file_pointer(s)
 the strtok is to remove the \n character from the left side file, the
 string pulled out of the file is then assigned to item i of the array */
 int i = 0;
@@ -250,7 +250,7 @@ if(fp_output == NULL){
 then concatenates the left and right strings together */
 int j = 0;
 while(j < left_total){
-    /* this if statement checks if there is a blank line
+    /*   this if statement checks if there is a blank line
     if there is, then it doesnt append a space to the line */
     if(strncmp(left_items[j], "\n", 2) == 0){
         strcat(left_items[j], right_items[j]);
--- padding.c
+++ padding.c
@@ -3,8 +3,50 @@
 #include<unistd.h>
 #include<string.h>
 
-int test(FILE fp_left_copy, FILE rp_right_copy, size_t left_total_copy, size_t right_total_copy)
+int padding(size_t left_total_copy, size_t right_total_copy, char* argv[]) //as its just a pointer, only need a copy???
 {
-//printf all the values passed to make sure it works
+/*
+   left_total_copy:   size of items in left file
+   right_total_copy: size of items in right file
+*/
+
+char test [1] = "\n";
+
+if(left_total_copy > right_total_copy){
+    //which file is big/smallest
+    size_t largest_file = left_total_copy;
+    size_t smallest_file = right_total_copy;
+    //create file pointer
+    FILE* smaller_file = fopen(argv[2], "a");
+    //not sure append returns a null, but futureproofing?
+    if(smaller_file == NULL){ 
+        printf("error padding bytes into %s\n", argv[2]);
+    }
+    while(smallest_file < largest_file){
+        printf("line %zu added \n", smallest_file);
+        printf("debug, largest_file is %zu\n", largest_file);
+        fputc(test[0], smaller_file);
+        smallest_file++;
+    }
+    fclose(smaller_file);
+}
+else{
+    size_t largest_file = right_total_copy;
+    size_t smallest_file = left_total_copy;
+    FILE* smaller_file = fopen(argv[1], "a");
+    if(smaller_file == NULL){
+        printf("error padding bytes into %s\n", argv[1]);
+    }
+     while(smallest_file < largest_file){
+        printf("line %zu added \n", smallest_file);
+        printf("debug, largest_file is %zu\n", largest_file);
+        fputc(test[0], smaller_file);
+        smallest_file++;
+     }
+     fclose(smaller_file);
+}
+
+
+
 return 0;
 }
