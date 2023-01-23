9c0d58b6a6ec5804adc058132be0c6121eee406e
lewisrobson <50459012+lewisrobson@users.noreply.github.com>
2023-01-23 00:25:32 +0000

    more stuff added into padding, woo
    updates, my head hurts and i need to get some sleep.

--- padding.c
+++ padding.c
@@ -10,10 +10,11 @@ int padding(size_t left_total_copy, size_t right_total_copy, char* argv[]) //as 
    right_total_copy: size of items in right file
 */
 
-char test [1] = "\n";
+char test [5] = "\n";
 
 if(left_total_copy > right_total_copy){
     //which file is big/smallest
+    printf("in main\n");
     size_t largest_file = left_total_copy;
     size_t smallest_file = right_total_copy;
     //create file pointer
@@ -28,24 +29,33 @@ if(left_total_copy > right_total_copy){
         fputc(test[0], smaller_file);
         smallest_file++;
     }
+
     fclose(smaller_file);
 }
-else{
+
+
+if(left_total_copy < right_total_copy){
+    printf("left total is %zu right is %zu\n", left_total_copy, right_total_copy);
     size_t largest_file = right_total_copy;
     size_t smallest_file = left_total_copy;
     FILE* smaller_file = fopen(argv[1], "a");
-    if(smaller_file == NULL){
+    /*if(smaller_file == NULL){
         printf("error padding bytes into %s\n", argv[1]);
-    }
-     while(smallest_file < largest_file){
-        printf("line %zu added \n", smallest_file);
-        printf("debug, largest_file is %zu\n", largest_file);
+    } */
+    printf("debug, largest_file is %zu\n", largest_file);
+     while(left_total_copy < right_total_copy){
         fputc(test[0], smaller_file);
-        smallest_file++;
+        left_total_copy++;
+        printf("line %zu added \n", left_total_copy);
+        if(left_total_copy == right_total_copy){
+            /* if this next line isnt in this while loop
+            it causes a segmentation fault, it doesnt do
+            it for the if statement above, i dont get it
+            my head hurts.*/
+            FILE* smaller_file = fopen(argv[1], "a");
+        }
      }
-     fclose(smaller_file);
 }
-
 
 
 return 0;
